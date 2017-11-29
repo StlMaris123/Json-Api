c0ec559023b40e54287071f20bf1487a93b19634
StlMaris123 <stellamarisnjage@gmail.com>
2017-11-29 11:40:58 +0300

    generate controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 
 class User < ApplicationRecord
 
-  has_many :questions, :foreign_key => "question_id", inverse_of: :asker
-  has_many :answers, :foreign_key => "user_id",   inverse_of: :answerer
+  has_many :questions#, inverse_of: :asker
+  has_many :answers#, inverse_of: :answerer
 
 end
