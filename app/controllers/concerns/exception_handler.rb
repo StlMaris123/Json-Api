376d800a1823836e8371bf969b376b89776b97b9
StlMaris123 <stellamarisnjage@gmail.com>
2017-12-05 09:38:22 +0300

    implement questions

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -2,12 +2,12 @@ module ExceptionHandler
   extend ActiveSupport::Concern
 
   included do
-    resque_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message  }, :not_found)
     end
 
-    resque_from ActiveRecord::RecordInvalid do |e|
-      json_response({message: e.message}, :unprocessed_entity)
+    rescue_from ActiveRecord::RecordInvalid do |e|
+      json_response({ message: e.message  }, :unprocessable_entity)
     end
   end
 end
