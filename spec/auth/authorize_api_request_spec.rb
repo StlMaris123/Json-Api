e6062cb85240e9d469bbbe1be7f4c1f5b7053e2a
StlMaris123 <stellamarisnjage@gmail.com>
2017-12-07 11:13:08 +0300

    define specs for token authorization

---
+++ spec/auth/authorize_api_request_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe AuthorizeApiRequest do
+  let(:tenant) { create(:tenant) }
+  #Mock Authorization header
+  let(:header) { {'Authorization' => token_generator(tenant.id)} }
+  #Invalid request subject
+  subject(:invalid_request_obj) { decsribed_class.new({}) }
+  #valid request subject
+  subject(:request_obj) { described_class.new(header) }
+
+  describe '#call' do
+    context 'when valid request' do
+      it 'returns tenant object' do
+        result = request_obj.call
+        expect(result[:tenant]).to eq(tenant)
+      end
+    end
+
+    context 'when invalid request' do
+      context 'when missing token' do
+        it 'raises a MisssingToken error' do
+          expect { invalid_request_obj.call }
+          .to raise_error(ExceptionHandler::MissingToken, 'Missing Token')
+        end
+      end
+
+      context 'when invalid token' do
+        subject(:invalid_request_obj) do
+          #custom helper method
+          described_class.new('Authorization' => token_generator(5))
+        end
+
+        it 'raises an invalidToken error' do
+          expect { invalid_request_obj.call }
+          .to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
+        end
+      end
+
+      context ' when token is expired' do
+        let(:header) {{ 'Authorization' => expired_token_generator(tenant.id) }}
+        subject(:request_obj) { described_class.new(header) }
+
+        it 'raises ExceptionHandler::ExpiredSignature error' do
+          expect { request_obj.call }
+          .to raise_error(
+            ExceptionHandler::InvalidToken,
+            /Signature has expired/
+          )
+        end
+      end
+    end
+  end
+end
+          
